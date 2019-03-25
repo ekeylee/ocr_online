25d3a0395d8cd526ff7711aba68a3eb5d8ea652e
李益达 <ekey.lee@gmail.com>
2019-03-25 16:54:32 +0800

    all

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ocr_online
+OCR Online
+===============
+website:
+http://ocr.ekeylee.com
---
+++ application/index/controller/Index.php
@@ -0,0 +1,50 @@
+<?php
+namespace app\index\controller;
+
+use think\Request;
+
+
+class Index
+{
+  /**
+   * 首页
+   */
+  public function index()
+  {
+    return view();
+  }
+
+  /**
+   * 获取文件
+   */
+  public function getFile()
+  {
+    $time = time();
+
+    switch ($_FILES['file']['type']) {
+      case 'image/jpeg':
+        $fix = 'jpeg';
+        break;
+      case 'image/png':
+        $fix = 'png';
+        break;
+    }
+    move_uploaded_file($_FILES['file']['tmp_name'], 'uploads' . DS . $time . '.' . $fix);
+    $str = $this->ocrapi($time . '.' . $fix);
+    return json($str);
+  }
+  public function ocrapi($file_name = '1553501389.png')
+  {
+    require '../vendor/autoload.php';
+    $aip = new \AipOcr('10834827', 'hvfbPZHmZ0G7sFPzUdxB6SZY', 'uH5RPQGNmZ2PGRpKcQKeTK7Yz6Dfqrg2');
+    $url = 'uploads' . DS . $file_name;
+    $image = file_get_contents($url);
+    $result = $aip->basicAccurate($image, ['detect_direction' => true]);
+    $str = '';
+    foreach ($result['words_result'] as $k => $v) {
+      $str .= $v['words'];
+    }
+    unlink($url);
+    return $str;
+  }
+}
